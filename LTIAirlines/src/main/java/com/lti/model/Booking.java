7c79631521ce5222ce95a93ebc1c19a2d29185de
Divya <divya111170@gmail.com>
2021-09-15 12:17:52 +0530

    second push

--- LTIAirlines/src/main/java/com/lti/model/Booking.java
+++ LTIAirlines/src/main/java/com/lti/model/Booking.java
@@ -3,6 +3,12 @@ package com.lti.model;
 public class Booking {
 	int bookingNo;
 	Flight flight;
+	
+	public Booking(int bookingNo, Flight flight) {
+		super();
+		this.bookingNo = bookingNo;
+		this.flight = flight;
+	}
 	public int getBookingNo() {
 		return bookingNo;
 	}
